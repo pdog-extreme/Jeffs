1e45a6a2815039323f2c660536b8307135c62d6e
woo457 <>
2019-07-09 16:57:38 +1000

    added Wmat. Can now get the weighted predictions

--- R/qualitative_models.R
+++ R/qualitative_models.R
@@ -71,10 +71,40 @@ Tmat <- function(A){
       Tmat_out[j,i] <- permanent(abs(A[-i,-j]))
     }
   }
-
   return(Tmat_out)
-
 }
+
+
+#' @title Weighted matrix (W).
+#' @rdname Wmat
+#' @param A A square adjacenty matrix.
+#' @param Adjmatrix Adjoint matrix if already calculated. Default is NULL.
+#' @param Tmatrix Tmat if already calculated. This can be slow and might make sense to reuse, rather than recalculate. Default is NULL.
+#' @details Compute the weighted matrix in a qualitative model.
+#' @return The weigthed matrix from a qualitative model.
+#' @export
+#' @examples
+#' n <- 13
+#' A <- matrix(c(-1, 0, 0, 1, 1, 0, 0, -1, -1, -1, 0, 0, -1, 0, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0, 0, 1, -1, 0, 0, 0, 0, 0, -1, 1, 0, -1, 1, 1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 0, 1, 0, 0, -1, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 1, 1, -1, -1, -1, -1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, -1, 1, 0, 0, 0, 0,1, 0, 0, 0, 0, 0, 1, -1, -1, 0, 1, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1),n ,n ,byrow=TRUE)
+#' Wmat(A)
+Wmat <- function(A,Adjmatrix=NULL,Tmatrix=NULL,use_NA=FALSE){
+
+  if(nrow(A)!=ncol(A))stop('A must be a symetrical matrix')
+  if(is.null(Adjmatrix)){
+    Adjmatrix <- adjoint(A)
+  }
+
+  if(is.null(Tmatrix)){
+    Tmatrix <- Tmat(A)
+  }
+
+  Wmat_out <- abs(Adjmatrix/Tmatrix)
+  if(!use_NA)Wmat_out[is.na(Wmat_out)] <- 1
+
+  return(Wmat_out)
+}
+
+
 
 
 
